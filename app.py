89d6dd8d0d964dd9a51e8ad3a49feb787f5a4da6
James Park <julim99@gmail.com>
2023-01-05 05:57:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -60,3 +60,18 @@ if st.checkbox('Show dataframe'):
        columns=['a', 'b', 'c'])
 
     chart_data
+    
+    
+    import streamlit as st
+import pandas as pd
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected: ', option
