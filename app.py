500ba2643b8b769c5d51795082d756a543d47169
James Park <julim99@gmail.com>
2023-01-05 05:46:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -22,3 +22,9 @@ st.write(out)
 st.markdown('Streamlit is **_really_ cool**.')
 st.markdown(":green[$\sqrt{x^2+y^2}=1$] is a Pythagorean identity. :pencil:")
 st.markdown('this text is :red[colored red], and this is :blue[colored pencil]')
+
+st.latex(r'''
+    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+    \sum_{k=0}^{n-1} ar^k =
+    a \left(\frac{1-r^{n}}{1-r}\right)
+    ''')
