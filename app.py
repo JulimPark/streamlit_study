056b331ded59dd47e135cf6a7fba18f6f4a7d407
James Park <julim99@gmail.com>
2023-01-05 05:39:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 
 import streamlit as st
+import pandas as pd
+
+
 a = [100,300,50]
 st.bar_chart(a)
 b = [1,2,3]
@@ -7,8 +10,12 @@ b = [1,2,3]
 st.bar_chart(b)
 
 c= [4,5,6]
+jj = {'a':a, 'b':b,'c':c}
+
+out = pd.DataFrame(jj)
 
 st.bar_chart(c)
 st.write('this is mouth')
 st.write('Hello **world**!')
+st.write(out)
 
