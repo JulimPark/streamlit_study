6b829a9d349a746e1525f842ca903610dc2e1faa
James Park <julim99@gmail.com>
2023-01-05 19:53:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -107,3 +107,23 @@ for i in range(100):
   time.sleep(0.1)
 
 '...and now we\'re done!'
+
+
+
+
+import streamlit as st
+
+@st.cache  # 👈 This function will be cached
+def my_slow_function(arg1, arg2):
+    # Do something really slow in here!
+    return the_output
+
+
+
+with st.container():
+   st.write("This is inside the container")
+
+   # You can call any Streamlit command, including custom components:
+   st.bar_chart(np.random.randn(50, 3))
+
+st.write("This is outside the container")
