58b8022e57d09c918ee018eccc893f9ea58b9dab
James Park <julim99@gmail.com>
2023-01-05 23:18:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ st.write('현재 응시자는 '+stu_name+'입니다')
 test_num = st.text_input('*:red[시험지코드]를 입력하세요 ', '0001')
 st.write('현재 시험지는 '+test_num+'입니다')
 
+question_num = st.text_input(':green[문항 수]를 입력하세요', 20)
+st.write('문항 수는'+question_num+'입니다')
 
 no1 = st.radio(
     '1번 문항의 정답을 입력하세요.',('1','2','3','4','5')
